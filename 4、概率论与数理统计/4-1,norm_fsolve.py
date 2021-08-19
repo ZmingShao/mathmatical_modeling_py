bebabec8c76cfbb15fd6f6dd2e2ba54042bd624e
SergeyLiu <1762529161@qq.com>
2021-08-19 16:13:15 +0800

    整理了第四章部分算法

--- 4、概率论与数理统计/4-1,norm_fsolve.py
+++ 4、概率论与数理统计/4-1,norm_fsolve.py
@@ -1,6 +1,6 @@
 #程序文件Pex4_1.py
-from scipy.stats import norm
-from scipy.optimize import fsolve
+from scipy.stats import norm    # 正态分布
+from scipy.optimize import fsolve   # 解方程
 print("p=",norm.cdf(6,3,5)-norm.cdf(2,3,5))
 f=lambda c: norm.cdf(2*c,3,5)-norm.cdf(-3*c,3,5)-0.6
 print("c=",fsolve(f,0))
